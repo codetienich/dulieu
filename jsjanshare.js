36710cb0d942bf4faf0a8f7a3baf191bf3d0f569
codetienich <95487967+codetienich@users.noreply.github.com>
2022-01-19 21:02:24 +0700

    Update jsjanshare.js

--- jsjanshare.js
+++ jsjanshare.js
@@ -1,4 +1,190 @@
 "use strict";
+
+
+var w      = parseInt(el('chieurongcrop').value);
+  var h      = parseInt(el('chieucaocrop').value);
+  var x      = parseInt(el('vitri-ngang').value);
+  var y      = parseInt(el('vitri-doc').value);
+  var r      = parseInt(el('doxoay').value);
+  var ratio  = w/h;
+
+  function updateData(){
+    w      = parseInt(el('chieurongcrop').value);
+    h      = parseInt(el('chieucaocrop').value);
+    x      = parseInt(el('vitri-ngang').value);
+    y      = parseInt(el('vitri-doc').value);
+    r      = parseInt(el('doxoay').value);
+    ratio  = w/h;
+  }
+  // DOM LOADED
+  window.addEventListener('DOMContentLoaded', function () {
+    var image = el('image');
+    var input = el('inputIMG');
+    var imgout = el('imgout');
+ 
+    var $modal = $('#imgModal');
+    var cropper;
+
+    // show/hide modal
+    $modal.on('shown.bs.modal', function () {
+      // tính lại các thông số
+      updateData();
+      console.log(w);
+
+      cropper = new Cropper(image, {
+        aspectRatio: ratio,
+        viewMode: 1,
+      });
+    }).on('hidden.bs.modal', function () {
+      cropper.destroy();
+      cropper = null;
+    });
+
+    // input change
+    input.addEventListener('change',function(){
+      document.getElementById('chonanh').textContent = 'Chọn ảnh khác';
+
+      // cập nhật lại data
+      updateData();
+      console.log(w+'-'+h);
+
+      // destroy exists cropper
+      cropper.destroy();
+      cropper = null;
+
+      var file = this.files[0];  
+      var image = document.getElementById('image');
+          image.src = URL.createObjectURL(file);
+
+      var done = function (url) {
+            input.value = '';
+            image.src = url;
+          };
+                    
+      var reader;
+      var file;
+      var url;
+
+      if (URL) {
+        done(URL.createObjectURL(file));
+      } else if (FileReader) {
+        reader = new FileReader();
+        reader.onload = function (e) {
+          done(reader.result);
+        };
+        reader.readAsDataURL(file);
+      }
+
+      // callback Cropper
+      cropper = new Cropper(image, {
+        aspectRatio: ratio,
+        viewMode: 1,
+      });
+
+      document.getElementById('cropanh-container').classList.remove("d-none");
+    });  // end change
+    
+
+    // Crop ảnh
+    document.getElementById('crop').addEventListener('click',function(){
+      this.textContent = 'Cắt ảnh...';
+      document.getElementById('upanh').textContent = 'Chọn ảnh khác';
+
+
+      var croppedCanvas = cropper.getCroppedCanvas();
+
+
+      croppedCanvas.toBlob(function(blob) {
+          var url = URL.createObjectURL(blob);
+          el('anhcrop').src = url;
+		  el('maunen').value = url;
+      });
+
+
+
+      setTimeout(function(){
+        document.getElementById('crop').textContent = 'Cắt ảnh';
+        // ẩn modal
+        $modal.modal('hide');
+      },40);      
+    });
+
+
+    document.getElementById('rotate-left').addEventListener('click',function(){
+      cropper.rotate(-90);
+    });
+
+    document.getElementById('rotate-right').addEventListener('click',function(){
+      cropper.rotate(90);
+    });
+
+    document.getElementById('flip-h').addEventListener('click',function(){
+      cropper.scale(-1, 1); // Flip horizontal
+    });
+
+    document.getElementById('flip-v').addEventListener('click',function(){
+      cropper.scale(1, -1); // Flip vertical
+    });
+
+    // change imgout style
+    var style = document.getElementsByName('style');
+    for (var i = 0, length = style.length; i < length; i++) {
+      style[i].addEventListener('click',function(){
+          el('data-overlay').value = this.value;
+          el('vitri-ngang').value       = this.getAttribute('vitri-ngang');
+          el('vitri-doc').value       = this.getAttribute('vitri-doc');
+          el('doxoay').value       = this.getAttribute('doxoay');
+          el('chieurongcrop').value       = this.getAttribute('chieurongcrop');
+          el('chieucaocrop').value       = this.getAttribute('chieucaocrop');
+          el('canvas').width       = this.getAttribute('chieurong_anhxuat');
+          el('canvas').height      = this.getAttribute('chieudai_anhxuat');
+      });
+    }
+
+
+    // dragable scroller-type
+    const slider = document.querySelector('.scroller-type');
+    let mouseDown = false;
+    let startX, scrollLeft;
+
+    let startDragging = function (e) {
+      mouseDown = true;
+      startX = e.pageX - slider.offsetLeft;
+      scrollLeft = slider.scrollLeft;
+    };
+    let stopDragging = function (event) {
+      mouseDown = false;
+    };
+
+    slider.addEventListener('mousemove', (e) => {
+      e.preventDefault();
+      if(!mouseDown) { return; }
+      const x = e.pageX - slider.offsetLeft;
+      const scroll = x - startX;
+      slider.scrollLeft = scrollLeft - scroll;
+    });
+
+    // Add the event listeners
+    slider.addEventListener('mousedown', startDragging, false);
+    slider.addEventListener('mouseup', stopDragging, false);
+    slider.addEventListener('mouseleave', stopDragging, false);
+
+
+  }); // end DOM
+
+  // active scroller-type mauanh
+    var mauanhs = document.querySelectorAll('.scroller-type .mauanh');
+    for(var i = 0; i< mauanhs.length; i ++) {
+      mauanhs[i].addEventListener('click',function(){
+        for (var j = 0; j< mauanhs.length; j ++) {
+          mauanhs[j].classList.remove('active');
+        }
+        this.classList.add('active');
+      });
+    }
+
+
+
 function el(e) {
     return document.getElementById(e)
 }
