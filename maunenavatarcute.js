c258688f148fe7bc99b7f5cadedf4320f41c6d48
codetienich <95487967+codetienich@users.noreply.github.com>
2022-02-05 11:20:23 +0700

    Add files via upload

---
+++ maunenavatarcute.js
@@ -0,0 +1,29 @@
+var chonkieu = [
+{
+	maunen:"https://lh3.googleusercontent.com/-TFdxDQc5zqE/Yf09UhiUfUI/AAAAAAAAA3E/7Q8oE0jjA6Y3kdccMrBBFToUqNvS1xUSwCNcBGAsYHQ/s0/cute1.png",
+},
+    
+{
+	maunen:"https://lh3.googleusercontent.com/-_W_ZQT_wO0o/Yf09Un49WTI/AAAAAAAAA28/q9q0c4bY7KQU7Fr-J1eB8hFY29ddx6gDACNcBGAsYHQ/s0/cute2.png",
+},
+    
+{
+	maunen:"https://lh3.googleusercontent.com/-H6cyq0ln3jc/Yf09UgMoRCI/AAAAAAAAA3A/HZUHCF4Y61YXT5I4Ne6pauWgnjZcTuHrQCNcBGAsYHQ/s0/cute3.png",
+},
+    
+{
+	maunen:"https://lh3.googleusercontent.com/-hpiFPSOCn4A/Yf09VvY8qbI/AAAAAAAAA3I/eHEpF-HOcGY2YAjKYd8iP6DvriIHnhySQCNcBGAsYHQ/s0/cute4.png",
+},
+    {
+	maunen:"https://lh3.googleusercontent.com/-FPUg1ulB9c8/Yf09WFPqMKI/AAAAAAAAA3M/bKSqOEtCyFctHKxhuhz-DBqfWgmk4uX_gCNcBGAsYHQ/s0/cute5.png",
+},
+    {
+	maunen:"https://lh3.googleusercontent.com/-8p-v1D_xFJA/Yf09WffF2YI/AAAAAAAAA3Q/UwMnvlfiiNE7MS1_6S_DLupBcgmqnQ5AACNcBGAsYHQ/s0/cute6.png",
+},
+{
+	maunen:"https://lh3.googleusercontent.com/-XHPwa_uTTag/Yf09WrWlIwI/AAAAAAAAA3U/T9bFqXLKY6I0equrfm6Lyv9hiSMPNKl3QCNcBGAsYHQ/s0/cute7.png",
+
+		
+	}	
+    
+];
